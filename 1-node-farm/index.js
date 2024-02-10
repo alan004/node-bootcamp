2d11e09d75eaeaccfb59b2462586f069c1686083
Alan Abilio <alandocarmoabilio@gmail.com>
2024-02-09 21:44:15 -0300

    section 2.16

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -32,23 +32,54 @@ const url = require('url');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); //array
 const dataProducts = JSON.parse(data); // json.parse transforma nossa json(que é tudo string) em um objeto
 
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  return output;
+}
+
+//ler os templates antes de ter as requisições
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8'); 
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8'); 
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8'); 
+
+
+//função chamada quando é recebida a requisição
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  (pathName === '/overview' || pathName === '/') 
-  ? (res.end('This is the overview')) 
-  : (pathName ==='/api')
-  ? (      
-      res.writeHead(200, {'Content-type': 'application/json'}),
-      res.end(data)
-    )
-  : (pathName === '/product') 
-  ? (res.end('This is the Product')) 
-  : (res.writeHead(404, {
-    'Content-type': 'text/html',
-  }),
-  res.end('<h1>Not found page</h1>'));
+  const {query, pathname} = url.parse(req.url, true)
+
+    //OVERVIEW 
+      if (pathname === '/overview' || pathname === '/') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardsHtml = dataProducts.map(e => replaceTemplate(templateCard, e)).join('')
+        const output = templateOverview.replace('{%PRODUCT_CARD%}', cardsHtml)
+        res.end(output)
+        // API
+      } else if (pathname === '/api' && req.method === 'GET') {
+        res.writeHead(200, {'Content-type': 'application/json'}),
+        res.end(data)
+        // PRODUCT
+      } else if  (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product =  dataProducts[query.id];
+        const output = replaceTemplate(templateProduct, product)
+        res.end(output)
+        // 404 PAGE
+      } else {
+        res.writeHead(404, {
+          'Content-type': 'text/html',
+        }),
+        res.end('<h1>Not found page</h1>')
+      }
 })
 
 server.listen(8000, () => {
   console.log('Listening to requests on port 8000');
-});
+})
